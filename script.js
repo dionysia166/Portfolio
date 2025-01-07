ab8d8a719adf217a866680758dadb97fa8ea55b8
dionysia166 <soula.voutou@mail.mcgill.ca>
2025-01-06 20:06:54 -0500

    Add button to scroll to top

--- script.js
+++ script.js
@@ -61,3 +61,18 @@ AOS.init({
   once: true,
 });
 
+
+// Show the button when the user scrolls down
+window.onscroll = function () {
+  let button = document.getElementById("back-to-top");
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+};
+
+// Scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
